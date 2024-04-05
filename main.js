1409ac1b441bd4cc100c9025a4be342b132a07ec
Murugesan E <114680526+MDMATHAN16@users.noreply.github.com>
2024-04-05 16:07:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,279 @@
+const connect= document.getElementById('connectButton')
+const registerProperty_btn= document.getElementById('registerProperty')
+const buyProperty_btn= document.getElementById('buyProperty')
+const getPropertyDetails_btn= document.getElementById('getPropertyDetails')
+
+
+const show=()=>{
+    const val1=parseInt(document.getElementById('input_val1').value);
+const val2=document.getElementById('input_val2').value;
+    console.log(typeof val1);
+    console.log(typeof val2);
+}
+
+let web3; // Declare a variable for the web3 instance
+let contract; 
+let accountName;
+connect.addEventListener("click", async () => {
+  console.log(window);
+  if (window.ethereum) { // Check for Metamask
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" });
+      web3 = new Web3(window.ethereum);
+      connect.textContent = "Connected";
+      connect.disabled = true;
+      accountName = prompt('Please enter a name for your Account:');
+      console.log('Account name:', accountName);
+    
+
+      // Create a contract instance using the ABI and contract address
+      const contractAddress = "0xA89A7986462F5B3BbCcA27B6DB899b43819329f8"; // Replace with the actual address 0x936a0788d061a001Ba4Ee975207C89644429F740
+      const ABI =[
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "uint256",
+                    "name": "id",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "price",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "newOwner",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "location",
+                    "type": "string"
+                }
+            ],
+            "name": "PropertyBought",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "uint256",
+                    "name": "id",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "price",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "location",
+                    "type": "string"
+                }
+            ],
+            "name": "PropertyRegistered",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_propertyId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "buyProperty",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "fetchData",
+            "outputs": [
+                {
+                    "components": [
+                        {
+                            "internalType": "uint256",
+                            "name": "id",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "string",
+                            "name": "name",
+                            "type": "string"
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "price",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "address",
+                            "name": "owner",
+                            "type": "address"
+                        },
+                        {
+                            "internalType": "string",
+                            "name": "location",
+                            "type": "string"
+                        }
+                    ],
+                    "internalType": "struct RealEstate.Property[]",
+                    "name": "",
+                    "type": "tuple[]"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "properties",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "id",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "price",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "internalType": "string",
+                    "name": "location",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_price",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_location",
+                    "type": "string"
+                }
+            ],
+            "name": "registerProperty",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
+      contract = new web3.eth.Contract(ABI, contractAddress);
+    
+    } catch (error) {
+      console.error(error);
+      alert("Error connecting to Metamask!");
+    }
+  } else {
+    alert("Please install Metamask!");
+  }
+});
+async function registerProperty(){
+    const price=parseInt(document.getElementById('input_val1').value);
+    const location=document.getElementById('input_val2').value;
+    contract.methods.registerProperty(accountName, price, location)
+    .estimateGas({ from: ethereum.selectedAddress })
+    .then((gasAmount) => {
+        contract.methods.registerProperty(accountName, price, location)
+            .send({ from: ethereum.selectedAddress, gas: gasAmount })
+            .then((receipt) => {
+                console.log('Property registered:', receipt);
+                getPropertyDetails();
+            })
+            .catch((error) => {
+                console.error('Error registering property:', error);
+            });
+    })
+    .catch((error) => {
+        console.error('Error estimating gas:', error);
+    });
+}
+
+async function buyProperty(){
+    const pId=parseInt(document.getElementById('input_val3').value);
+        await contract.methods.buyProperty(pId).send({ from: ethereum.selectedAddress })
+        .then(alert('transaction successful!'))
+        .catch(alert('Error buying the property!!'));
+}
+
+async function getPropertyDetails(){
+    const pId=parseInt(document.getElementById('input_val4').value);
+    try{
+        const res=await contract.methods.fetchData().call();
+        let l = res.length;
+        for(let i=0;i<l;i++)
+        {
+            create(res[i]['price'],res[i]['name'],res[i]['location']);
+            console.log('name: ',res[i]['name'],' id : ',res[i]['id'],' location : ' ,res[i]['location'],' price : ',res[i]['price'],' owner : ',res[i]['owner']);
+        }
+    }
+    catch(err){
+    alert('Error fetching property details!!')}
+}
---
+++ script.js
@@ -0,0 +1,48 @@
+function create(pri,name,loc)
+{
+    // Create the section element
+var section = document.createElement("section");
+
+// Set the id attribute of the image
+var image = document.createElement("img");
+image.setAttribute("id", "image");
+image.setAttribute("src", "image.jpeg");
+image.setAttribute("alt", "");
+section.appendChild(image);
+
+// Create and set the heading element
+var heading = document.createElement("h1");
+heading.textContent = loc;
+section.appendChild(heading);
+
+// Create and set the owner paragraph
+var ownerParagraph = document.createElement("p");
+ownerParagraph.setAttribute("class", "owner");
+ownerParagraph.innerHTML = "Owner: <span>"+name+"</span>";
+section.appendChild(ownerParagraph);
+
+// Create and set the details paragraph
+var detailsParagraph = document.createElement("p");
+detailsParagraph.innerHTML = "<span>size</span> <span>type</span> <span>Active</span>";
+section.appendChild(detailsParagraph);
+
+// Create and set the description paragraph
+var descriptionParagraph = document.createElement("p");
+descriptionParagraph.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, eaq tecto sequi, repellendus deserunt nam!";
+section.appendChild(descriptionParagraph);
+
+// Create and set the price paragraph
+var priceParagraph = document.createElement("p");
+priceParagraph.innerHTML = "Price: <span>$"+pri+"</span>";
+section.appendChild(priceParagraph);
+
+// Create the buy button
+var buyButton = document.createElement("button");
+buyButton.textContent = "BUY";
+buyButton.style.backgroundColor = "green";
+section.appendChild(buyButton);
+
+// Append the section to the document body or any desired parent element
+document.body.appendChild(section);
+
+}
